e306c96d3a89dba51158b7a9e70f8053c081db6c
carlos <radix_10339@hotmail.com>
2019-09-18 10:06:55 -0500

    html pure removed with pug

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ var express = require('express'),
   serveStatic = require('serve-static'),
   app = express(),
   port = process.env.PORT || 8000,
-  html_template_path = path.join(__dirname, 'html'),
   static_path = path.join(__dirname, 'static');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.route('/').get(function (req, res) {
-	res.sendFile(path.join(html_template_path, 'index.html'))
-})
-
-app.route('/index').get(function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index')
 })
 
 app.use('/static', express.static(static_path));
