cf323eae81dcb6beab21b1c24648aa93cef1d255
carlos <radix_10339@hotmail.com>
2019-09-18 09:15:38 -0500

    implement url with pug

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 8000,
   html_template_path = path.join(__dirname, 'html'),
-  static_path = path.join(__dirname, 'static')
+  static_path = path.join(__dirname, 'static');
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.route('/').get(function (req, res) {
 	res.sendFile(path.join(html_template_path, 'index.html'))
+})
+
+app.route('/index').get(function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.use('/static', express.static(static_path));
