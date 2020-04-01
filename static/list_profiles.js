002d1c98664bce83e9a7c1bcfe898e734c961f83
carlos <radix_10339@hotmail.com>
2020-04-01 17:55:28 -0600

    add js for retrieve data for index

---
+++ static/list_profiles.js
@@ -0,0 +1,21 @@
+
+const listProfiles = async () => {
+    const response = await fetch('http://' + API_URL + '/profiles');
+    const json_response = await response.json();
+    let htmlIndex = "";
+    let htmlImages = "";
+    json_response.forEach((element, index) => {
+        activeList = !index ? `class="active"` : "";
+        activeImage = !index ? `class="item active"` : `class="item"`;
+        const templateIndex = `<li ${activeList} data-target="#myCarousel" data-slide-to="${index}"></li>`;
+        const templateImages = `<div ${activeImage}><img src="${element.imagen}" alt="${element.imagen}"></div>`;
+        htmlIndex += templateIndex;
+        htmlImages += templateImages;
+    });
+    let imageIndicator = document.getElementsByClassName("carousel-indicators")[0];
+    imageIndicator.innerHTML = htmlIndex;
+    let carrouselInner = document.getElementsByClassName("carousel-inner")[0];
+    carrouselInner.innerHTML = htmlImages;
+}
+
+window.addEventListener("load", listProfiles);
