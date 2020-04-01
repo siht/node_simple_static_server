68aff540b296cf56e88f61c48d77aa6c0a75ef9b
carlos <radix_10339@hotmail.com>
2020-04-01 12:39:19 -0600

    add modal to know status of upload

--- static/app.js
+++ static/app.js
@@ -4,8 +4,24 @@ const formToObject = elements => [].reduce.call(elements, (data, element) => {
   return data;
 }, {});
 
+function showMessageModal(message){
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
+  var span = document.getElementsByClassName("close")[0];
+  var contentMessage = document.getElementsByClassName("message")[0];
+  contentMessage.innerHTML = message;
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
 const submitProfile = async () => {
-  let form_object = formToObject($("#submitForm").serializeArray());
+  let form_object = formToObject($("#submitForm").serializeArray()); // TODO: quit jquery
   const response = await fetch('http://' + API_URL + '/profile/new/', {
     method: 'POST',
     body: JSON.stringify(form_object),
@@ -13,16 +29,30 @@ const submitProfile = async () => {
       'Content-Type': 'application/json'
     }
   });
-  const newProfile = await response.json();
-  const form = new FormData();
-  let image = $("#imagen_id")[0].files[0];
-  form.append("image", image);
-  let dataSettingsToUploadApi = {
-    method: "POST",
-    body: form
-  };
-  const responseImage = await fetch(
-    'http://' + API_URL + '/profile/' + newProfile._id + '/upload-image',
-    dataSettingsToUploadApi
-  );
+  try{
+    const newProfile = await response.json();
+    const form = new FormData();
+    let image = $("#imagen_id")[0].files[0]; // TODO: quit jquery
+    form.append("image", image);
+    let dataSettingsToUploadApi = {
+      method: "POST",
+      body: form
+    };
+    const responseImage = await fetch(
+      'http://' + API_URL + '/profile/' + newProfile._id + '/upload-image',
+      dataSettingsToUploadApi
+    );
+  }catch(error){
+    showMessageModal(error);
+  }finally{
+    document.getElementById("submitForm").reset();
+    showMessageModal("se ha subido con éxito el perfil");
+  }
+  
+}
+
+var submitButton = document.getElementById("submitButton");
+
+submitButton.onclick = async () => {
+  await submitProfile();
 }
