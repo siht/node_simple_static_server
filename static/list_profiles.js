574a41e473eee0a7baed2e212464cb4fbe562c85
carlos <radix_10339@hotmail.com>
2020-04-02 01:03:25 -0600

    fix error

--- static/list_profiles.js
+++ static/list_profiles.js
@@ -6,16 +6,18 @@ const listProfiles = async () => {
     let htmlImages = "";
     json_response.forEach((element, index) => {
         activeList = !index ? `class="active"` : "";
-        activeImage = !index ? `class="item active"` : `class="item"`;
+        activeImage = !index ? `class="carousel-item active"` : `class="carousel-item"`;
         const templateIndex = `<li ${activeList} data-target="#myCarousel" data-slide-to="${index}"></li>`;
         const templateImages = `<div ${activeImage}><img src="${element.imagen}" alt="${element.titulo}"></div>`;
         htmlIndex += templateIndex;
         htmlImages += templateImages;
     });
-    let imageIndicator = document.getElementsByClassName("carousel-indicators")[0];
-    imageIndicator.innerHTML = htmlIndex;
     let carrouselInner = document.getElementsByClassName("carousel-inner")[0];
+    let imageIndicator = document.getElementsByClassName("carousel-indicators")[0];
     carrouselInner.innerHTML = htmlImages;
+    imageIndicator.innerHTML = htmlIndex;
 }
 
-window.addEventListener("load", listProfiles);
+window.onload = async () => {
+    await listProfiles();
+}
